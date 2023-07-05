3fadc651f681152a4e4c85bf01f83d7d34db7852
AlexOsypenko <alexwork778@gmail.com>
2023-07-05 13:03:07 +0300

    - Add Body anottation

--- data/src/main/java/com/example/data/backend/models/AuthApiService.kt
+++ data/src/main/java/com/example/data/backend/models/AuthApiService.kt
@@ -3,12 +3,13 @@ package com.example.data.backend.models
 import com.example.data.backend.models.responses.Tokens
 import com.example.domain.utils.Endpoints
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthApiService {
 
     @POST (Endpoints.REFRESH_ENDPOINT)
     suspend fun refreshToken (
-        token: String,
+        @Body token: String,
     ) : Response<Tokens>
 }
