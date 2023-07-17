6ed0935c81225fcfcc6eea5077ef72ca1b684ec3
AlexOsypenko <alexwork778@gmail.com>
2023-07-17 13:28:35 +0300

    - Added changes to SwitchButton.kt;

--- app/src/main/java/com/example/blanball/presentation/views/components/switches/SwitchButton.kt
+++ app/src/main/java/com/example/blanball/presentation/views/components/switches/SwitchButton.kt
@@ -16,5 +16,5 @@ fun SwitchButton(
     selected: Boolean,
     onCheckedChange: (Boolean) -> Unit,
 ) {
-    Switch(checked = selected, onCheckedChange = onCheckedChange, colors = SwitchDefaults.colors(checkedThumbColor = Color.White, checkedTrackColor = mainGreen), modifier = Modifier.size(32.dp, 16.dp))
+    Switch(checked = selected, onCheckedChange = onCheckedChange, colors = SwitchDefaults.colors(checkedThumbColor = Color.White, checkedTrackColor = mainGreen, checkedTrackAlpha = 1f), modifier = Modifier.size(32.dp, 16.dp))
 }
