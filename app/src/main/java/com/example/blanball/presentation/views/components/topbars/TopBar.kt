79dca583c5423adb5163e6f5fbfe97e27898ce35
AlexOsypenko <alexwork778@gmail.com>
2023-08-01 19:28:57 +0300

    Top Bar Scaffold created

---
+++ app/src/main/java/com/example/blanball/presentation/views/components/topbars/TopBar.kt
@@ -0,0 +1,32 @@
+package com.example.blanball.presentation.views.components.topbars
+
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.blanball.R
+
+@Composable
+fun TopBar(
+    navController: NavController,
+    onNavIconClicked: () -> Unit,
+) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    TopAppBar (
+        title = { Text(text = stringResource(id = R.string.blanball))},
+        navigationIcon = {
+            IconButton(onClick = onNavIconClicked) {
+                Icon(imageVector = Icons.Default.Menu, contentDescription = null)
+            }
+        }
+    )
+}
