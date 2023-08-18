d44924fd1b3f85fa46372d7f761b3fbdb6abb0a3
AlexOsypenko <alexwork778@gmail.com>
2023-08-18 23:11:50 +0300

    - Added validation to Found An Error Screen; - Added UI changes to Found An Error Screen; - Added new integers constant; - Added new string;

--- app/src/main/java/com/example/blanball/presentation/views/screens/foundanerror/FoundAnErrorScreen.kt
+++ app/src/main/java/com/example/blanball/presentation/views/screens/foundanerror/FoundAnErrorScreen.kt
@@ -11,6 +11,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Divider
@@ -23,6 +26,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -37,6 +41,8 @@ import com.example.blanball.presentation.theme.shapes
 import com.example.blanball.presentation.theme.successGreen
 import com.example.blanball.presentation.theme.typography
 import com.example.blanball.presentation.views.components.textinputs.DefaultTextInput
+import com.example.blanball.utils.ext.isNotValidErrorTopicField
+import com.example.blanball.utils.ext.isValidErrorTopicField
 
 @Composable
 fun FoundAnErrorScreen(
@@ -49,16 +55,20 @@ fun FoundAnErrorScreen(
         modifier = Modifier.padding(paddingValues)
     ) {
         (state as? FoundAnErrorScreenMainContract.State)?.let {
-            Column(modifier = Modifier.padding(20.dp)) {
+            Column(
+                modifier = Modifier
+                    .padding(start = 20.dp, top = 20.dp, end = 20.dp)
+                    .verticalScroll(rememberScrollState()),
+            ) {
                 Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(
-                    text = stringResource(R.string.report_an_error_in_the_app),
-                    fontSize = 16.sp,
-                    lineHeight = 24.sp,
-                    style = typography.h3,
-                    fontWeight = FontWeight(600),
-                    color = primaryDark,
-                )
+                    Text(
+                        text = stringResource(R.string.report_an_error_in_the_app),
+                        fontSize = 16.sp,
+                        lineHeight = 24.sp,
+                        style = typography.h3,
+                        fontWeight = FontWeight(600),
+                        color = primaryDark,
+                    )
                     Spacer(modifier = Modifier.weight(1f))
                     Icon(
                         painter = painterResource(id = R.drawable.ic_error),
@@ -69,34 +79,54 @@ fun FoundAnErrorScreen(
                 Spacer(modifier = Modifier.size(20.dp))
                 DefaultTextInput(
                     modifier = Modifier.fillMaxWidth(),
-                    trailingIcon = { Icon(
-                        painter = painterResource(R.drawable.ic_target),
-                        contentDescription = null,
-                        tint = secondaryNavy
-                        ) },
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_target),
+                            contentDescription = null,
+                            tint = secondaryNavy
+                        )
+                    },
                     labelResId = R.string.what_is_the_error_related_to,
                     state = it,
-                    value = state.errorTopicText.value ,
-                    onValueChange = { state.errorTopicText.value = it},
-                    transformation = VisualTransformation.None
+                    value = state.errorTopicText.value,
+                    onValueChange = { state.errorTopicText.value = it },
+                    transformation = VisualTransformation.None,
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Next
+                    ),
+                    isError = when {
+                        it.errorTopicText.value.isNotValidErrorTopicField() -> true
+                        else -> false
+                    }, // TODO()
+                    errorMessage = when {
+                        it.errorTopicText.value.isNotValidErrorTopicField() -> stringResource(R.string.validation_text_error_topic)
+                        else -> {
+                            ("")
+                        }
+                    }
                 )
                 Spacer(modifier = Modifier.size(16.dp))
                 DefaultTextInput(
                     modifier = Modifier.fillMaxWidth(),
                     trailingIcon = { Icon(
+                        modifier = Modifier.clickable { }, //TODO()
                         painter = painterResource(R.drawable.ic_clip),
                         contentDescription = null,
                         tint = secondaryNavy
-                    ) },
+                    )
+                    },
                     textFieldModifier = Modifier
                         .fillMaxWidth()
                         .height(104.dp),
-                    state =  it,
+                    state = it,
                     isSingleLine = false,
                     value = state.errorDescriptionText.value,
                     onValueChange = { state.errorDescriptionText.value = it },
                     transformation = VisualTransformation.None,
                     labelResId = R.string.when_and_where_did_you_encounter_the_error,
+                    keyboardOptions = KeyboardOptions(
+                        imeAction = ImeAction.Done
+                    ),
                 )
                 Spacer(modifier = Modifier.size(18.dp))
                 Row (verticalAlignment = Alignment.CenterVertically) {
@@ -142,6 +172,8 @@ fun FoundAnErrorScreen(
                     )
                     Spacer(modifier = Modifier.weight(1f))
                     Button(
+                        enabled = it.errorTopicText.value.isValidErrorTopicField()
+                                && it.errorTopicText.value.isNotEmpty(),
                         onClick = { /*TODO*/ },
                         shape = shapes.medium,
                         colors = ButtonDefaults.buttonColors(
