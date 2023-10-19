10d8bc301d6cdaa7a9a54cb5d924eeabb8b4fca1
AlexOsypenko <alexwork778@gmail.com>
2023-10-19 12:10:18 +0300

    - Added changes to SideBar Button UI;

--- app/src/main/java/com/example/blanball/presentation/views/components/topbars/TopBar.kt
+++ app/src/main/java/com/example/blanball/presentation/views/components/topbars/TopBar.kt
@@ -1,19 +1,26 @@
 package com.example.blanball.presentation.views.components.topbars
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.blanball.R
+import com.example.blanball.presentation.theme.bgLight
 import com.example.blanball.presentation.theme.primaryDark
+import com.example.blanball.presentation.theme.shapes
 
 @Composable
 fun TopBar(
@@ -32,7 +39,18 @@ fun TopBar(
         contentColor = primaryDark,
         navigationIcon = {
             IconButton(onClick = onNavIconClicked) {
-                Icon(imageVector = Icons.Default.Menu, contentDescription = null)
+                Box(
+                    modifier = Modifier
+                        .size(32.dp)
+                        .background(color = bgLight, shape = shapes.medium),
+                ) {
+                    Icon(
+                        modifier = Modifier.align(Alignment.Center),
+                        painter = painterResource(id = R.drawable.ic_sidebar_menu),
+                        tint = primaryDark,
+                        contentDescription = null
+                    )
+                }
             }
         }
     )
