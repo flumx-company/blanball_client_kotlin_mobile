e34de71366866dfa23062b21a531b1127d909519
AlexOsypenko <alexwork778@gmail.com>
2024-01-09 19:16:02 +0200

    - Code refactoring && navigation optimization;

--- app/src/main/java/com/example/blanball/presentation/views/components/bottomnavbars/BottomNavBar.kt
+++ app/src/main/java/com/example/blanball/presentation/views/components/bottomnavbars/BottomNavBar.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material.BottomNavigation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
