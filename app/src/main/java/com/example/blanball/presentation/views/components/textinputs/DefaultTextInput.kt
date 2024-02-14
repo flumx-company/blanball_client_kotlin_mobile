f6116b70627aabb1e78fa54a33bb0eab40dc5c8c
AlexOsypenko <alexwork778@gmail.com>
2024-02-14 23:06:37 +0200

    - Create new custom text input;

--- app/src/main/java/com/example/blanball/presentation/views/components/textinputs/DefaultTextInput.kt
+++ app/src/main/java/com/example/blanball/presentation/views/components/textinputs/DefaultTextInput.kt
@@ -1,26 +1,32 @@
-    package com.example.blanball.presentation.views.components.textinputs
+package com.example.blanball.presentation.views.components.textinputs
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.blanball.R
 import com.example.blanball.presentation.data.UiState
 import com.example.blanball.presentation.theme.defaultLightGray
 import com.example.blanball.presentation.theme.errorRed
@@ -30,45 +36,71 @@ import com.example.blanball.presentation.theme.selectedDarkGray
 import com.example.blanball.presentation.theme.shapes
 import com.example.blanball.presentation.theme.typography
 
-    @Composable
-    fun DefaultTextInput(
-        modifier: Modifier = Modifier.fillMaxWidth(),
-        textFieldModifier: Modifier = Modifier.fillMaxWidth(),
-        labelResId: Int,
-        state: UiState,
-        value: String,
-        isError: Boolean = false,
-        onValueChange: (String) -> Unit,
-        transformation: VisualTransformation,
-        keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-        keyboardActions: KeyboardActions = KeyboardActions.Default,
-        errorMessage: String = "",
-        trailingIcon: @Composable (() -> Unit)? = null,
-        leadingIcon: @Composable (() -> Unit)? = null,
-        isSingleLine: Boolean = true,
-        readOnly: Boolean = false,
-        enabled: Boolean = true,
-        interactionSource: MutableInteractionSource = MutableInteractionSource(),
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun DefaultTextInput(
+    modifier: Modifier = Modifier.fillMaxWidth(),
+    textFieldModifier: Modifier = Modifier.padding(top = 5.dp).height(44.dp).fillMaxWidth(),
+    labelResId: Int,
+    state: UiState,
+    value: String,
+    isError: Boolean = false,
+    onValueChange: (String) -> Unit,
+    transformation: VisualTransformation,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    errorMessage: String = "",
+    trailingIcon: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    isSingleLine: Boolean = true,
+    readOnly: Boolean = false,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = MutableInteractionSource(),
+) {
+    val interactionSource by remember { mutableStateOf(interactionSource) }
+    val isTextFieldFocused by interactionSource.collectIsFocusedAsState()
+    Column(
+        modifier = modifier.animateContentSize().fillMaxSize()
     ) {
-        Column(modifier = modifier
-            .animateContentSize()
-            .fillMaxSize()) {
-            OutlinedTextField(
-                modifier = textFieldModifier,
-                textStyle =  TextStyle(
-                    fontFamily = FontFamily(
-                        Font(R.font.inter),
-                    ),
-                    fontSize = 13.sp,
-                    fontWeight = FontWeight(400),
-                    lineHeight = 24.sp,
-                ),
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = textFieldModifier,
+            singleLine = isSingleLine,
+            interactionSource = interactionSource,
+            enabled = enabled,
+            visualTransformation = transformation,
+            keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
+            readOnly = readOnly,
+        ) { innerTextField ->
+            TextFieldDefaults.OutlinedTextFieldDecorationBox(
                 value = value,
-                interactionSource = interactionSource,
-                onValueChange = onValueChange,
-                visualTransformation = transformation,
+                innerTextField = innerTextField,
+                enabled = enabled,
                 singleLine = isSingleLine,
-                readOnly = readOnly,
+                leadingIcon = leadingIcon,
+                border = {
+                    Box(
+                        modifier = Modifier.border(
+                            width = 1.dp,
+                            color = (
+                                    when {
+                                        (isTextFieldFocused) -> selectedDarkGray
+                                        (isError) -> errorRed
+                                        else -> selectedDarkGray
+                                    }),
+                            shape = shapes.medium
+                        )
+                    )
+                },
+                trailingIcon = trailingIcon,
+                visualTransformation = VisualTransformation.None,
+                interactionSource = interactionSource,
+                contentPadding = TextFieldDefaults.textFieldWithLabelPadding(
+                    top = 0.dp, bottom = 0.dp
+                ),
+                isError = isError,
                 label = {
                     Text(
                         stringResource(
@@ -81,7 +113,6 @@ import com.example.blanball.presentation.theme.typography
                         lineHeight = 16.sp,
                     )
                 },
-                shape = shapes.small,
                 colors = TextFieldDefaults.outlinedTextFieldColors(
                     unfocusedBorderColor = defaultLightGray,
                     focusedBorderColor = selectedDarkGray,
@@ -89,16 +120,11 @@ import com.example.blanball.presentation.theme.typography
                     errorBorderColor = errorRed,
                     focusedLabelColor = primaryDark,
                     cursorColor = mainGreen,
-                ),
-                keyboardOptions = keyboardOptions,
-                keyboardActions = keyboardActions,
-                isError = isError,
-                trailingIcon = trailingIcon,
-                leadingIcon = leadingIcon,
-                enabled = enabled,
+                )
             )
-            if (isError) {
-                Text(text = errorMessage, style = typography.h6, color = errorRed)
-            }
+        }
+        if (isError) {
+            Text(text = errorMessage, style = typography.h6, color = errorRed)
         }
     }
+}
