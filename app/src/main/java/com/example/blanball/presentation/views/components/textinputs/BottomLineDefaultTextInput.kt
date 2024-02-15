8c5c58ecd8b67b5d8b0e518970d834ebed9a52b5
AlexOsypenko <alexwork778@gmail.com>
2024-02-15 17:59:55 +0200

    - Change BottomLineDefaultTextInput to new custom input style;

--- app/src/main/java/com/example/blanball/presentation/views/components/textinputs/BottomLineDefaultTextInput.kt
+++ app/src/main/java/com/example/blanball/presentation/views/components/textinputs/BottomLineDefaultTextInput.kt
@@ -1,15 +1,23 @@
 package com.example.blanball.presentation.views.components.textinputs
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
@@ -17,20 +25,26 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextMotion
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.blanball.R
 import com.example.blanball.presentation.data.UiState
+import com.example.blanball.presentation.theme.borderPrimary
 import com.example.blanball.presentation.theme.defaultLightGray
 import com.example.blanball.presentation.theme.errorRed
 import com.example.blanball.presentation.theme.mainGreen
 import com.example.blanball.presentation.theme.primaryDark
 import com.example.blanball.presentation.theme.selectedDarkGray
-import com.example.blanball.presentation.theme.shapes
 import com.example.blanball.presentation.theme.typography
+import com.example.blanball.presentation.views.components.modifiers.BorderPosition
+import com.example.blanball.presentation.views.components.modifiers.customBorder
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun BottomLineDefaultTextInput(
     modifier: Modifier = Modifier,
+    textFieldModifier: Modifier = Modifier,
     labelResId: Int,
     state: UiState,
     value: String,
@@ -41,46 +55,76 @@ fun BottomLineDefaultTextInput(
     keyboardActions: KeyboardActions = KeyboardActions.Default,
     errorMessage: String = "",
 ) {
+    val interactionSource by remember { mutableStateOf(MutableInteractionSource()) }
+    val isTextFieldFocused by interactionSource.collectIsFocusedAsState()
     Box(modifier = modifier,) {
         Column() {
-        TextField(
-            value = value,
-            onValueChange = onValueChange,
-            visualTransformation = transformation,
-            singleLine = true,
-            textStyle =  TextStyle(
-                fontFamily = FontFamily(
-                    Font(R.font.inter),
-                ),
-                fontSize = 13.sp,
-                fontWeight = FontWeight(400),
-                lineHeight = 24.sp,
-            ),
-            label = {
-                Text(
-                    stringResource(
-                        id = labelResId
-                    ),
+            BasicTextField(
+                modifier = textFieldModifier.padding(top = 5.dp).height(44.dp).fillMaxWidth(),
+                value = value,
+                onValueChange = onValueChange,
+                singleLine = true,
+                interactionSource = interactionSource,
+                enabled = true,
+                textStyle = TextStyle(
                     color = primaryDark,
-                    style = typography.h6,
                     fontSize = 13.sp,
                     fontWeight = FontWeight(400),
-                    lineHeight = 16.sp,
+                    fontFamily = FontFamily(Font(R.font.inter)),
+                    lineHeight = 24.sp,
+                    textMotion = TextMotion.Animated,
+                ),
+                visualTransformation = transformation,
+                keyboardOptions = keyboardOptions,
+                keyboardActions = keyboardActions,
+            ) { innerTextField ->
+                TextFieldDefaults.OutlinedTextFieldDecorationBox(
+                    value = value,
+                    innerTextField = innerTextField,
+                    enabled = true,
+                    singleLine = true,
+                    border = {
+                        Box(
+                            modifier = Modifier.customBorder(
+                                color = (
+                                        when {
+                                            (isTextFieldFocused && !isError) -> selectedDarkGray
+                                            (isError) -> errorRed
+                                            else -> borderPrimary
+                                        }),
+                                position = BorderPosition.Bottom,
+                                strokeWidth = 1.dp
+                            )
+                        )
+                    },
+                    visualTransformation = VisualTransformation.None,
+                    interactionSource = interactionSource,
+                    contentPadding = TextFieldDefaults.textFieldWithLabelPadding(
+                        top = 0.dp, bottom = 0.dp
+                    ),
+                    isError = isError,
+                    label = {
+                        Text(
+                            stringResource(
+                                id = labelResId
+                            ),
+                            color = primaryDark,
+                            style = typography.h6,
+                            fontSize = 13.sp,
+                            fontWeight = FontWeight(400),
+                            lineHeight = 16.sp,
+                        )
+                    },
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        unfocusedBorderColor = defaultLightGray,
+                        focusedBorderColor = selectedDarkGray,
+                        textColor = primaryDark,
+                        errorBorderColor = errorRed,
+                        focusedLabelColor = primaryDark,
+                        cursorColor = mainGreen,
+                    )
                 )
-            },
-            shape = shapes.small,
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                unfocusedBorderColor = defaultLightGray,
-                focusedBorderColor = selectedDarkGray,
-                textColor = Color.Black,
-                errorBorderColor = errorRed,
-                focusedLabelColor = primaryDark,
-                cursorColor = mainGreen,
-            ),
-            keyboardOptions = keyboardOptions,
-            keyboardActions = keyboardActions,
-            isError = isError,
-        )
+            }
             if (isError) {
                 Text(text = errorMessage, style = typography.h6, color = errorRed)
             }
