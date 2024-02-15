11a101f5c69260b7fb1820885c3cc801e4b840e5
AlexOsypenko <alexwork778@gmail.com>
2024-02-15 17:48:59 +0200

    - Change CodeTextInput to new custom input style;

--- app/src/main/java/com/example/blanball/presentation/views/components/textinputs/CodeTextInput.kt
+++ app/src/main/java/com/example/blanball/presentation/views/components/textinputs/CodeTextInput.kt
@@ -1,21 +1,28 @@
 package com.example.blanball.presentation.views.components.textinputs
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -24,10 +31,16 @@ import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.blanball.R
 import com.example.blanball.presentation.data.UiState
 import com.example.blanball.presentation.theme.defaultLightGray
 import com.example.blanball.presentation.theme.errorRed
@@ -37,11 +50,12 @@ import com.example.blanball.presentation.theme.selectedDarkGray
 import com.example.blanball.presentation.theme.shapes
 import com.example.blanball.presentation.theme.typography
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun <T : UiState> CodeTextInput(
     state: T,
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
+    textFieldModifier: Modifier = Modifier,
     errorMessage: String = "",
     isError: Boolean? = null,
     enabled: Boolean = true,
@@ -49,21 +63,22 @@ fun <T : UiState> CodeTextInput(
     onCodeChange: (T, Int, String) -> Unit,
     onDoneAction: () -> Unit = {},
 ) {
-    val centerAlignedTextStyle = TextStyle(
-        textAlign = TextAlign.Center
-    )
+    val interactionSource by remember { mutableStateOf(MutableInteractionSource()) }
+    val isTextFieldFocused by interactionSource.collectIsFocusedAsState()
     val localFocusManager = LocalFocusManager.current
     val focusRequesters = remember {
         List(5) { FocusRequester() }
     }
-    Column(modifier = Modifier.animateContentSize()) {
-        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+    Column(modifier = modifier.animateContentSize().fillMaxWidth()) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
             repeat(5) { i ->
-                OutlinedTextField(
-                    isError = isError ?: false,
-                    textStyle = centerAlignedTextStyle,
-                    modifier = modifier
+                BasicTextField(
+                    modifier = textFieldModifier
                         .weight(1f)
+                        .height(44.dp)
                         .fillMaxWidth()
                         .onKeyEvent {
                             if (it.key == Key.Backspace && i > 0) {
@@ -85,17 +100,19 @@ fun <T : UiState> CodeTextInput(
                             newValue.length == 1 && i < 4 -> focusRequesters[i + 1].requestFocus()
                         }
                     },
-                    label = { Text("") },
+                    singleLine = true,
+                    interactionSource = interactionSource,
                     enabled = enabled,
-                    visualTransformation = VisualTransformation.None,
-                    shape = shapes.small,
-                    colors = TextFieldDefaults.outlinedTextFieldColors(
-                        unfocusedBorderColor = defaultLightGray,
-                        focusedBorderColor = selectedDarkGray,
-                        textColor = primaryDark,
-                        errorBorderColor = errorRed,
-                        cursorColor = mainGreen,
+                    textStyle = TextStyle(
+                        color = primaryDark,
+                        fontSize = 13.sp,
+                        fontWeight = FontWeight(400),
+                        fontFamily = FontFamily(Font(R.font.inter)),
+                        lineHeight = 24.sp,
+                        textMotion = TextMotion.Animated,
+                        textAlign = TextAlign.Center,
                     ),
+                    visualTransformation = VisualTransformation.None,
                     keyboardOptions = KeyboardOptions(
                         imeAction = when (i) {
                             4 -> ImeAction.Done
@@ -106,7 +123,42 @@ fun <T : UiState> CodeTextInput(
                         4 -> KeyboardActions(onDone = { localFocusManager.clearFocus(); onDoneAction() })
                         else -> KeyboardActions.Default
                     },
-                )
+                ) { innerTextField ->
+
+                    TextFieldDefaults.OutlinedTextFieldDecorationBox(
+                        innerTextField = innerTextField,
+                        enabled = enabled,
+                        value = codeText(state)[i].value,
+                        border = {
+                            Box(
+                                modifier = Modifier.border(
+                                    width = 1.dp,
+                                    color = (
+                                            when {
+                                                (isTextFieldFocused && !isError!!) -> selectedDarkGray
+                                                (isError == true) -> errorRed
+                                                else -> defaultLightGray
+                                            }),
+                                    shape = shapes.medium
+                                )
+                            )
+                        },
+                        visualTransformation = VisualTransformation.None,
+                        interactionSource = interactionSource,
+                        contentPadding = TextFieldDefaults.textFieldWithLabelPadding(
+                            top = 0.dp, bottom = 0.dp
+                        ),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            unfocusedBorderColor = defaultLightGray,
+                            focusedBorderColor = selectedDarkGray,
+                            textColor = primaryDark,
+                            errorBorderColor = errorRed,
+                            focusedLabelColor = primaryDark,
+                            cursorColor = mainGreen,
+                        ),
+                        singleLine = true,
+                    )
+                }
             }
         }
         if (isError == true) {
