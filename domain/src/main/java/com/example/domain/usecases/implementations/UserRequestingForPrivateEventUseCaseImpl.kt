964a5fb750bedf1b895f956f9396344214cb5418
AlexOsypenko <alexwork778@gmail.com>
2024-03-26 14:43:59 +0200

    - Added UseCase logic;

--- domain/src/main/java/com/example/domain/usecases/implementations/UserRequestingForPrivateEventUseCaseImpl.kt
+++ domain/src/main/java/com/example/domain/usecases/implementations/UserRequestingForPrivateEventUseCaseImpl.kt
@@ -1,5 +1,6 @@
 package com.example.domain.usecases.implementations
 
+import com.example.domain.entity.results.AcceptOrDiscardParticipationResult
 import com.example.domain.entity.results.GetPrivateEventRequestListResult
 import com.example.domain.repository.AppRepository
 import com.example.domain.usecases.interfaces.UserRequestingForPrivateEventUseCase
@@ -9,5 +10,14 @@ class UserRequestingForPrivateEventUseCaseImpl @Inject constructor(val appReposi
     UserRequestingForPrivateEventUseCase {
     override suspend fun executeGetPrivateEventRequestList(eventId: Int): GetPrivateEventRequestListResult {
         return appRepository.getPrivateEventRequestList(eventId = eventId)
+    }
+
+    override suspend fun executeAcceptingDiscardingEventRequest(
+        isAcceptEventRequest: Boolean,
+        ids: List<Int>
+    ): AcceptOrDiscardParticipationResult {
+        return appRepository.acceptOrDiscardParticipation(
+            isAcceptEventRequest = isAcceptEventRequest, ids = ids,
+        )
     }
 }
--- domain/src/main/java/com/example/domain/usecases/interfaces/UserRequestingForPrivateEventUseCase.kt
+++ domain/src/main/java/com/example/domain/usecases/interfaces/UserRequestingForPrivateEventUseCase.kt
@@ -1,7 +1,9 @@
 package com.example.domain.usecases.interfaces
 
+import com.example.domain.entity.results.AcceptOrDiscardParticipationResult
 import com.example.domain.entity.results.GetPrivateEventRequestListResult
 
 interface UserRequestingForPrivateEventUseCase {
     suspend fun executeGetPrivateEventRequestList(eventId: Int) : GetPrivateEventRequestListResult
+    suspend fun executeAcceptingDiscardingEventRequest(isAcceptEventRequest: Boolean, ids: List<Int>): AcceptOrDiscardParticipationResult
 }
