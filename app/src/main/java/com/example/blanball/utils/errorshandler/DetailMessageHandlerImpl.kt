df3d3f766946c587b198379202c12940c6a29aa7
AlexOsypenko <alexwork778@gmail.com>
2024-03-28 13:13:47 +0200

    - Added changes;

--- app/src/main/java/com/example/blanball/utils/errorshandler/DetailMessageHandlerImpl.kt
+++ app/src/main/java/com/example/blanball/utils/errorshandler/DetailMessageHandlerImpl.kt
@@ -1,15 +1,19 @@
 package com.example.blanball.utils.errorshandler
 
+import android.app.Application
+import com.example.blanball.R
 import javax.inject.Inject
 
-class DetailMessageHandlerImpl @Inject constructor() : DetailMessageHandler {
+class DetailMessageHandlerImpl @Inject constructor(
+    val application: Application
+) : DetailMessageHandler {
     override suspend fun handleErrorMessage(message: String): String {
         val errorFields = ResponseDetailsMessages::class.members
         for (field in errorFields ) {
             if (field.name == message) {
-                return field.call(ResponseDetailsMessages()) as String
+                return field.call(ResponseDetailsMessages()).toString()
             }
         }
-        return "Невідома помилка"
+        return application.getString(R.string.unknown_error)
     }
 }
